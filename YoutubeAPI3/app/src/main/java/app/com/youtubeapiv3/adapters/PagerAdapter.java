959035cbcfeafcf86d38d40d5d348fb7cddf0df0
Sylwestrone <sylwesterantosik1@gmail.com>
2018-02-15 22:51:14 +0100

    Add files via upload

---
+++ YoutubeAPI3/app/src/main/java/app/com/youtubeapiv3/adapters/PagerAdapter.java
@@ -0,0 +1,34 @@
+package app.com.youtubeapiv3.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import app.com.youtubeapiv3.fragments.ChannelFragment;
+import app.com.youtubeapiv3.fragments.PlayListFragment;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new ChannelFragment();
+            case 1:
+                return new PlayListFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
---
+++ YoutubeAPI3/app/src/main/java/app/com/youtubeapiv3/interfaces/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package app.com.youtubeapiv3.interfaces;
+
+
+import app.com.youtubeapiv3.models.YoutubeDataModel;
+
+public interface OnItemClickListener {
+    void onItemClick(YoutubeDataModel item);
+
+}
